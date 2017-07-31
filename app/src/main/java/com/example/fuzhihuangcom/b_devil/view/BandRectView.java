076afc0b7c32392e28312267de1fa84f9b94309b
devil <372294896@qq.com>
2017-07-31 22:47:28 +0800

    '理解音频条形图，并注释'

--- app/src/main/java/com/example/fuzhihuangcom/b_devil/view/BandRectView.java
+++ app/src/main/java/com/example/fuzhihuangcom/b_devil/view/BandRectView.java
@@ -2,8 +2,10 @@ package com.example.fuzhihuangcom.b_devil.view;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.LinearGradient;
 import android.graphics.Paint;
+import android.graphics.Shader;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -16,8 +18,8 @@ public class BandRectView extends View {
     public static final String TAG = "MusicRectangleView";
     private Paint mPaint;
     public int mOffset = 10;
-    public int mRectWidth ;
-    public int mRectHeight ;
+    public int mRectWidth;
+    public int mRectHeight;
     private int mRectCount = 10;
     private float currentHeight;
     private int mWidth;
@@ -41,18 +43,25 @@ public class BandRectView extends View {
         mPaint = new Paint();
         mWidth = getWidth();
         mRectHeight = getHeight();
-        mRectWidth = (int)(mWidth*0.6/mRectCount);
-//        mLinearGradient = new LinearGradient(0,0,mRectWidth,mRectHeight, Color.YELLOW,Color.BLUE, Shader.TileMode.CLAMP);
-//        mPaint.setShader(mLinearGradient);
+        mRectWidth = (int) (mWidth * 0.6 / mRectCount);
+        // CLAMP的作用是如果渲染器超出原始边界范围，则会复制边缘颜色对超出范围的区域进行着色
+        mLinearGradient = new LinearGradient(0, 0, 0, mRectHeight, Color.YELLOW, Color.BLUE, Shader.TileMode.CLAMP);
+        mPaint.setShader(mLinearGradient);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        for (int i=0;i<mRectCount;i++){
-            currentHeight = (float)(Math.random()*mRectHeight);
-            canvas.drawRect((float)(mWidth*0.4/2+mRectWidth*i+mOffset),currentHeight,(float)(mWidth*0.4/2+mRectWidth*(i+1)),mRectHeight,mPaint);
-            // postInvalidateDelayed(300);
+        for (int i = 0; i < mRectCount; i++) {
+            currentHeight = (float) (Math.random() * mRectHeight);
+            canvas.drawRect((float) (mWidth * 0.4 / 2 + mRectWidth * i + mOffset)
+                    , currentHeight
+                    , (float) (mWidth * 0.4 / 2 + mRectWidth * (i + 1))
+                    , mRectHeight, mPaint);
+            // postInvalidateDelayed()方法非UI线程中使用，UI线程不能进行耗时操作，这里需要500毫秒，所以用此方法
+            postInvalidateDelayed(500);
+            // invalidate()方法只能在UI线中使用
+            // invalidate();
         }
     }
 }
